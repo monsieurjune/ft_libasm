b7bdb733e67cd40f99f5a718e8b9eb5fc90e2506
monsieurjune <june.monsieurjune@gmail.com>
2025-09-02 23:58:02 +0700

    perf(libasm): Use 'test' for check null, instead of 'cmp' in ft_strcpy()

--- lib/srcs/ft_strcpy.s
+++ lib/srcs/ft_strcpy.s
@@ -11,10 +11,12 @@ ft_strcpy:
     mov r8, rdi     ; copy s1 to s1-tmp
 
     .loop_start:
-        mov byte r9b, [rsi]     ; copy *s2 to tmp
-        mov byte [r8], r9b      ; copy tmp to *s1-tmp
-        cmp byte [rsi], 0       ; compare *s2 to 0
+        mov byte r9b, [rsi]     ; copy *s2 to s2-tmp
+        mov byte [r8], r9b      ; copy s2-tmp to *s1-tmp
+
+        test byte r9b, r9b      ; if s2-tmp == 0, ZF falg is set
         je .loop_end            ; jump to .loop_end if *s2 == 0
+
         inc r8                  ; s1-tmp++
         inc rsi                 ; s2++
         jmp .loop_start
