1315d1bae9c05701b5c5ab05f9fcd4e3e71f281c
monsieurjune <june.monsieurjune@gmail.com>
2025-09-04 06:29:14 +0700

    fix(libasm): Fix return value on ft_strcmp()
    Fix:
    - Fix signed bits disapear issue when do 'sub' operation to the return
      rgister 'a'
    
    Perf:
    - load value from memory at the start of loop to register, then use them
    - use 'movzx', instead of 'mov'. for preserve signed bit + both
      operation consume same amount of clocks when load from memory anyway

--- lib/srcs/ft_strcmp.s
+++ lib/srcs/ft_strcmp.s
@@ -8,27 +8,20 @@ global ft_strcmp
 ;   rsi = const char* s2
 
 ft_strcmp:
-    mov rax, 0      ; clean the rax register
-
     .loop_start:
-        mov byte r10b, [rdi]    ; copy *s1 to s1-tmp-0
+        movzx eax, byte [rdi]   ; copy *s1 to s1-tmp
+        movzx r8d, byte [rsi]   ; copy *s2 to s2-tmp
 
-        cmp byte r10b, [rsi]    ; compare s1-tmp-0 with *s2
-        jne .loop_end           ; jump if s1-tmp-0 != *s2
+        test eax, r8d           ; if s1-tmp or s2-tmp == 0, ZF is set
+        jz .loop_end            ; jump to .loop_end if ZF is set
 
-        test byte r10b, [rsi]   ; if s1-tmp-0 or *s2 == 0, ZF is set
-        jz .loop_both_zero      ; jump if ZF flag is set
+        cmp eax, r8d            ; compare s1-tmp to s2-tmp
+        jne .loop_end           ; jume to .loop_end if they aren't equal
 
         inc rdi                 ; s1++
         inc rsi                 ; s2++
         jmp .loop_start
 
     .loop_end:
-        mov byte r8b, [rdi]     ; copy *s1 to s1-tmp-1
-        mov byte r9b, [rsi]     ; copy *s2 to s2-tmp-0
-        sub r8d, r9d            ; s1-tmp-0 - s2-tmp-1 as int, avoid OF flag set
-        mov al, r8b             ; return 0th byte of result
-        ret
-
-    .loop_both_zero:
+        sub eax, r8d            ; ret = s1-tmp - s2-tmp
         ret
